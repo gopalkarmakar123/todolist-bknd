d21cdbd64897e8af349a46fb43f4ca0316bff63f
Gopal Karmakar <gopal.karmakara002@outlook.com>
2022-03-11 19:29:06 +0530

    set json to get method

--- app.js
+++ app.js
@@ -71,9 +71,9 @@ const serverCallback = async (req,res) => {
                             console.log(body);
                             var post = JSON.parse(body);
                             console.log("post" , post);
-                            await saveTodo(client,post).catch(console.error()).then(result =>{
-                                resp = {error_code: 0, message : "todo successfully saved.", data:post};
-                            });
+                            await saveTodo(client,post).catch(console.error());
+                            resp = {error_code: 0, message : "todo successfully saved.", data:post};
+                            
                             
                         });
                         break;
